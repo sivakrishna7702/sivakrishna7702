0572bbafbc901ed04b35dd4834d1434640dadec6
sivakrishna7702 <160828303+sivakrishna7702@users.noreply.github.com>
2024-04-09 20:16:17 +0530

    Create rectangleshap.py

---
+++ rectagleshap.py
@@ -0,0 +1,7 @@
+#creating rectangle shape with 5 rows and 3 columns
+for x in range(5):                 #outer loop iterating 5 times for rows
+   count = 0
+  while count < 3:                 #inner loop iteratinf 3 times as columns
+      print("*",end=" ")
+      count +=1
+  print("")
